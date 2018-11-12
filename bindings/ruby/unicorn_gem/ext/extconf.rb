90203d42ec5ccad1d755c4aa022253594cb5b2bb
misson20000 <xenotoad@xenotoad.net>
2018-11-11 17:33:03 -0800

    bindings/ruby: abort if unicorn library is not found

--- bindings/ruby/unicorn_gem/ext/extconf.rb
+++ bindings/ruby/unicorn_gem/ext/extconf.rb
@@ -3,6 +3,6 @@ require 'mkmf'
 extension_name = 'unicorn_engine'
 
 dir_config(extension_name)
-have_library('unicorn')
+have_library('unicorn') or abort('unicorn is missing')
 
 create_makefile(extension_name)
